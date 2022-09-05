5aa5aa6d012492c68fd8524353252a605df0e582
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2022-09-05 17:55:48 +0700

    Update urls.py

--- shorten_url/urls.py
+++ shorten_url/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
@@ -22,6 +21,7 @@ from drf_yasg.views import get_schema_view
 
 schema_view = get_schema_view(
     openapi.Info(
+        title='Shorten url',
         default_version='v1'
     ),
     public=True,
