4d640216875aca25dc2c9ada1c7f639ce4473681
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2023-04-08 10:40:50 +0700

    Update urls.py

--- shorten_url/urls.py
+++ shorten_url/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -37,3 +40,6 @@ urlpatterns = [
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     url('api/', include('apps.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
